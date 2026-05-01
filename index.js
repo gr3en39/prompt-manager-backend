4cb73af1c36bbb0e3029c7ba12218f654bb2de3d
gr3en39 <gr3en39@gmail.com>
2026-05-01 19:58:33 +0800

    Add password authentication with 2-day session expiry

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: (import.meta.env.VITE_API_URL || 'http://localhost:3001').replace(/\/$/, ''),
+  origin: '*',
   credentials: true
 }));
 
